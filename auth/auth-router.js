5dfdfc924b3b41242e88f0f76e03a11f69710f8a
Brennon Nuckols <brennuck@gmail.com>
2020-03-03 14:16:25 -0700

    class notes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.username = user.username;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -36,5 +38,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.get('/login', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ you: "can checkout any time you like, but you can never leave" })
+      } else {
+        res.status(200).json({ you: "logged out successfully" })
+      }
+    });
+  } else {
+    res.status(200).json({ bye: "felicia" });
+  }
+})
 
 module.exports = router;
